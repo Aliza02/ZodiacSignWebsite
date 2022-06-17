535e1fe8705cea29a720d2abc4c263bd8699ef22
Aliza02 <alizaanxari08@gmail.com>
2022-06-17 19:49:06 +0500

    zodiac structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import { DialogExampleComponent } from './dialog-example/dialog-example.component';
+import {ViewEncapsulation, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { filter, map, mergeMap, shareReplay } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches),shareReplay());
+    open=false;
+    
   title = 'material-test';
+  constructor(
+    private breakpointObserver: BreakpointObserver,){
+    
+    
+   
+  }
+//   openDialog()
+//   {
+//   let dialogref=this.dialog.open(DialogExampleComponent,{   width:'550px' , height:'200px',
+// data:{name:'aliza'}});
+//   dialogref.afterClosed().subscribe(result=>{
+//     console.log(`dialog result:${result}`);
+//   });
+//   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import { DialogExampleComponent } from './dialog-example/dialog-example.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { HeaderComponent } from './header/header.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogExampleComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatToolbarModule
+    ,MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
